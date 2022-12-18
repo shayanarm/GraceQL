6b96b11b462b6bcf274d7d0c69f24b949a596390
amneshia <amirshayan.armaghan@gmail.com>
2022-12-17 19:44:38 -0500

    Modify the SQL compiler to be replace throwables with the new Validation type

---
+++ src/main/scala/graceql/core/CompilationFramework.scala
@@ -0,0 +1,74 @@
+package graceql.core
+
+import scala.quoted.Quotes
+import graceql.data.Validated
+import scala.quoted.*
+
+trait CompilationFramework(using val q: Quotes) {
+    import q.reflect.*
+    import Validated.*
+
+    final type Result[+A] = Validated[String, A]
+
+    extension (tpe: Type[_])
+        def no: Type[_] =
+        tpe match
+            case '[Option[a]] => Type.of[a]
+            case _            => tpe
+        def typeRepr: TypeRepr = TypeRepr.of(using tpe)
+
+    extension (trep: TypeRepr)
+        def no: TypeRepr =
+        trep.asType match
+            case '[a] => Type.of[a].no.typeRepr
+
+    extension (errs: Seq[String])
+        def listString(headline: String = "Errors encountered"): Option[String] =
+        if errs.isEmpty then None
+        else
+            Some(
+            errs
+                .map(e => s"-  $e")
+                .mkString(
+                s"$headline:\n",
+                "\n",
+                ""
+                )
+            )
+    type Requirements <: BaseRequirements
+    trait BaseRequirements:
+        def apply[E, A](v: Result[A])(msg: => String = "Requirement failed"): A =
+        v match
+            case Valid(v) => v
+            case i: Invalid[_, _] => throw GraceException(i.errors.listString(msg).get)
+
+        def instance[T](using Type[T]): Expr[T] =
+        require(
+            Expr
+            .summon[T]
+            .toValid(s"Could not obtain an instance for ${Type.show[T]}")
+        )("Type Class instance not found")
+
+    val require: Requirements
+
+    def withImplicit[P, A](p: Expr[P])(
+        f: Expr[P ?=> A]
+    )(using Type[P], Type[A]): Expr[A] =
+        preprocess('{ $f(using $p) })
+
+    def preprocess[A](
+        e: Expr[A]
+    )(using ta: Type[A]): Expr[A] =
+        import graceql.quoted.CompileOps.*
+        val pipe =
+        inlineDefs andThen
+            betaReduceAll andThen
+            inlineDefs
+        pipe(e.asTerm).asExprOf[A]
+
+    def summonValid[T](using Type[T]): Result[Expr[T]] =
+        Expr.summon[T] match
+            case Some(enc) => enc.asValid
+            case None =>
+                s"Failed to obtain an instances of ${Type.show[T]}".err
+}
